6ced511c61bf62261af0214d37241b76628a4c4e
Reetesh Ranjan <reeteshr@outlook.com>
2013-12-24 08:29:11 -0500

    command line options structured and parsed using getopt for summarizer.

--- src/summarizer.c
+++ src/summarizer.c
@@ -6,16 +6,44 @@
 
 /* FUNCTIONS */
 
+void usage(const char* prog)
+{
+    fprintf(stderr, "Usage: %s -i <input-file> -r <ratio>\n", prog);
+    fprintf(stderr, "Usage: %s -h\n\n", prog);
+    fprintf(stderr, "input-file : the file to summarize\n");
+    fprintf(stderr, "     ratio : indicated using a percentage (without %%) sign\n");
+    fprintf(stderr, "        -h : print this help\n");
+}
+
 int
 main(int argc, char** argv)
 {
     lang_t     lang;
     article_t  article;
     status_t   status;
+    int        opt;
+    literal_t  file_name = NULL;
+    float      ratio = 0.0;
 
-    if(argc < 3) {
-        fprintf(stderr, "Invalid number of arguments\n");
-        return(-1);
+    while(-1 != (opt = getopt(argc, argv, "i:r:h"))) {
+        switch(opt) {
+            case 'i': file_name = optarg; break;
+            case 'r': ratio = atof(optarg)/100; break;
+            case 'h': usage(argv[0]); return(0);
+            default: usage(argv[0]); return(1);
+        }
+    }
+
+    if(NULL == file_name) {
+        fprintf(stderr, "No input file specified\n");
+        usage(argv[0]);
+        return(1);
+    }
+
+    if(0.0 == ratio) {
+        fprintf(stderr, "Ratio cannot be 0.0\n");
+        usage(argv[0]);
+        return(1);
     }
 
     status =
@@ -27,9 +55,9 @@ main(int argc, char** argv)
 
         article_init(&article) ||
 
-        parse_article(argv[1], &lang, &article) ||
+        parse_article(file_name, &lang, &article) ||
 
-        grade_article(&article, &lang, atof(argv[2])/100);
+        grade_article(&article, &lang, ratio);
 
     print_summary(&article);
 
